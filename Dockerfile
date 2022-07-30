cfbb753c27beab62a3d386d79d94cfcfe5571d0e
alexander berardi <alexanderberardi@alexanders-iMac.local>
2022-07-30 11:19:37 -0400

    adding __main__ to main.py

--- Dockerfile
+++ Dockerfile
@@ -7,3 +7,4 @@ RUN pip3 install -r requirements.txt
 COPY . /app
 EXPOSE ${PORT}
 
+CMD["python3", "main.py"]
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import os
+import uvicorn
 import strawberry
 from fastapi import FastAPI
 from strawberry.fastapi import GraphQLRouter
+from dotenv import load_dotenv
 
+load_dotenv(".env")
 
 @strawberry.type
 class Query:
@@ -20,3 +24,6 @@ app.include_router(graphql_app, prefix="/graphql")
 @app.get("/")
 async def root():
     return {"message": "Hello World!"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=os.environ["PORT"])
