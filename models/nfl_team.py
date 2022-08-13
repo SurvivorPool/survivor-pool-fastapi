6137d795fb92826f48e65370a4f4243da5901adc
alexberardi <alexmberardi@gmail.com>
2022-08-13 12:08:04 -0400

    Database refactor

--- models/nfl_team.py
+++ models/nfl_team.py
@@ -1,11 +1,11 @@
 import uuid
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Integer
 from sqlalchemy.dialects.postgresql import UUID
 from db.base_class import Base
 
 
 class NFLTeam(Base):
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    id = Column(Integer, nullable=False, unique=True, primary_key=True)
     abbreviation = Column(String, nullable=False)
     city_state = Column(String, nullable=False)
     full_name = Column(String, nullable=False)
